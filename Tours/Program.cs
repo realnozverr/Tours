211fd83726f91133af08799437dda00bd0441fb2
realnozverr <rubon.koko@gmail.com>
2026-01-27 17:41:01 +0700

    добавил хттп клиент

--- Tours/Program.cs
+++ Tours/Program.cs
@@ -9,6 +9,19 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"))
         .ConfigureWarnings(warnings => warnings.Ignore(Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.PendingModelChangesWarning)));
 
+var couchDbUri = new Uri(builder.Configuration.GetConnectionString("CouchDB")!);
+builder.Services.AddHttpClient("CouchDB", client =>
+{
+    client.BaseAddress = new Uri($"{couchDbUri.Scheme}://{couchDbUri.Host}:{couchDbUri.Port}");
+    
+    if (!string.IsNullOrEmpty(couchDbUri.UserInfo))
+    {
+        var authBytes = System.Text.Encoding.ASCII.GetBytes(couchDbUri.UserInfo);
+        client.DefaultRequestHeaders.Authorization = 
+            new System.Net.Http.Headers.AuthenticationHeaderValue("Basic", Convert.ToBase64String(authBytes));
+    }
+});
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
